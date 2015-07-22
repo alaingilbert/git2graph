67866b8e2e775a64ee75a006c5e4e4be747d462f
Alain Gilbert <alain.gilbert.15@gmail.com>
2015-07-22 16:06:27 -0700

    Add new test

--- git2graph_test.go
+++ git2graph_test.go
@@ -214,3 +214,47 @@ func Test5(t *testing.T) {
 	validateColumns(t, expectedColumns, out)
 	validatePaths(t, expectedPaths, out)
 }
+
+// 1
+// |\
+// | 2
+// |/|
+// 3 |
+// | 4
+// |/
+// 5
+func Test6(t *testing.T) {
+	// Initial input
+	inputNodes := make([]InputNode, 0)
+	inputNodes = append(inputNodes, InputNode{"1", []string{"3", "2"}})
+	inputNodes = append(inputNodes, InputNode{"2", []string{"3", "4"}})
+	inputNodes = append(inputNodes, InputNode{"3", []string{"5"}})
+	inputNodes = append(inputNodes, InputNode{"4", []string{"5"}})
+	inputNodes = append(inputNodes, InputNode{"5", []string{}})
+
+	out, _ := buildTree(inputNodes)
+
+	// Expected output
+	expectedColumns := []int{0, 1, 0, 1, 0}
+
+	expectedPaths := []map[string][]Point{
+		map[string][]Point{
+			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
+			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+		},
+		map[string][]Point{
+			"3": []Point{Point{1, 1, 0}, Point{0, 1, 3}, Point{0, 2, 0}},
+			"4": []Point{Point{1, 1, 0}, Point{1, 3, 0}},
+		},
+		map[string][]Point{
+			"5": []Point{Point{0, 2, 0}, Point{0, 4, 0}},
+		},
+		map[string][]Point{
+			"5": []Point{Point{1, 3, 0}, Point{1, 4, 1}, Point{0, 4, 0}},
+		},
+	}
+
+	// Validation
+	validateColumns(t, expectedColumns, out)
+	validatePaths(t, expectedPaths, out)
+}
