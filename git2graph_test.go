972943df8d18e7165dd56f991a84027d66c8b49a
Alain Gilbert <alain.gilbert.15@gmail.com>
2015-07-23 17:47:54 -0700

    prepare tests for colors

--- git2graph_test.go
+++ git2graph_test.go
@@ -13,20 +13,20 @@ func validateColumns(t *testing.T, expectedColumns []int, data []*OutputNode) {
 	}
 }
 
-func validatePaths(t *testing.T, expectedPaths []map[string][]Point, data []*OutputNode) {
+func validatePaths(t *testing.T, expectedPaths []map[string]Path, data []*OutputNode) {
 	for nodeIdx, node := range data {
 		for _, parentId := range node.Parents {
-			if len(node.ParentsPaths[parentId].Path) != len(expectedPaths[nodeIdx][parentId]) {
+			if len(node.ParentsPaths[parentId].Path) != len(expectedPaths[nodeIdx][parentId].Path) {
 				t.Fail()
-				t.Logf("Id: %s, Expected nb paths: %d, Actual nb paths: %d", node.Id, len(expectedPaths[nodeIdx][parentId]), len(node.ParentsPaths[parentId].Path))
+				t.Logf("Id: %s, Expected nb paths: %d, Actual nb paths: %d", node.Id, len(expectedPaths[nodeIdx][parentId].Path), len(node.ParentsPaths[parentId].Path))
 				t.Logf("Id: %s, Expected: %v, Actual: %v", node.Id, expectedPaths[nodeIdx][parentId], node.ParentsPaths[parentId])
 				return
 			}
 			for pathIdx, pathNode := range node.ParentsPaths[parentId].Path {
-				if pathNode != expectedPaths[nodeIdx][parentId][pathIdx] {
+				if pathNode != expectedPaths[nodeIdx][parentId].Path[pathIdx] {
 					t.Fail()
-					t.Logf("Id: %s, Expected path: %d, Actual path: %d", node.Id, expectedPaths[nodeIdx][parentId][pathIdx], pathNode)
-					t.Logf("Id: %s, Expected: %v, Actual: %v", node.Id, expectedPaths[nodeIdx][parentId], node.ParentsPaths[parentId])
+					t.Logf("Id: %s, Expected path: %d, Actual path: %d", node.Id, expectedPaths[nodeIdx][parentId].Path[pathIdx], pathNode)
+					t.Logf("Id: %s, Expected: %v, Actual: %v", node.Id, expectedPaths[nodeIdx][parentId].Path, node.ParentsPaths[parentId].Path)
 				}
 			}
 		}
@@ -50,12 +50,12 @@ func Test1(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 0, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"2": []Point{Point{0, 0, 0}, Point{0, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{0, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"3": []Point{Point{0, 1, 0}, Point{0, 2, 0}},
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 1, 0}, Point{0, 2, 0}}, ""},
 		},
 	}
 
@@ -80,12 +80,12 @@ func Test2(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
 		},
-		map[string][]Point{
-			"3": []Point{Point{1, 1, 0}, Point{1, 2, 1}, Point{0, 2, 0}},
+		map[string]Path{
+			"3": Path{"3", []Point{Point{1, 1, 0}, Point{1, 2, 1}, Point{0, 2, 0}}, ""},
 		},
 	}
 
@@ -111,13 +111,13 @@ func Test3(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"3": []Point{Point{1, 1, 0}, Point{1, 2, 1}, Point{0, 2, 0}},
+		map[string]Path{
+			"3": Path{"3", []Point{Point{1, 1, 0}, Point{1, 2, 1}, Point{0, 2, 0}}, ""},
 		},
 	}
 
@@ -150,20 +150,20 @@ func Test4(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0, 2, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{1, 1, 0}, Point{1, 4, 1}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{1, 1, 0}, Point{1, 4, 1}, Point{0, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{0, 2, 0}, Point{0, 4, 0}},
-			"4": []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 3, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{0, 2, 0}, Point{0, 4, 0}}, ""},
+			"4": Path{"4", []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{2, 3, 0}, Point{2, 4, 1}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{2, 3, 0}, Point{2, 4, 1}, Point{0, 4, 0}}, ""},
 		},
 	}
 
@@ -196,21 +196,21 @@ func Test5(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 2, 0, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"4": []Point{Point{0, 0, 0}, Point{0, 3, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"4": Path{"4", []Point{Point{0, 0, 0}, Point{0, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{1, 1, 0}, Point{1, 3, 1}, Point{0, 3, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{1, 1, 0}, Point{1, 3, 1}, Point{0, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{2, 2, 0}, Point{2, 3, 1}, Point{0, 3, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{2, 2, 0}, Point{2, 3, 1}, Point{0, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{0, 3, 0}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{0, 3, 0}, Point{0, 5, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{1, 4, 0}, Point{1, 5, 1}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{1, 4, 0}, Point{1, 5, 1}, Point{0, 5, 0}}, ""},
 		},
 	}
 
@@ -241,20 +241,20 @@ func Test6(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"3": []Point{Point{1, 1, 0}, Point{0, 1, 3}, Point{0, 2, 0}},
-			"4": []Point{Point{1, 1, 0}, Point{1, 3, 0}},
+		map[string]Path{
+			"3": Path{"3", []Point{Point{1, 1, 0}, Point{0, 1, 3}, Point{0, 2, 0}}, ""},
+			"4": Path{"4", []Point{Point{1, 1, 0}, Point{1, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{0, 2, 0}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{0, 2, 0}, Point{0, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{1, 3, 0}, Point{1, 4, 1}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{1, 3, 0}, Point{1, 4, 1}, Point{0, 4, 0}}, ""},
 		},
 	}
 
@@ -288,23 +288,23 @@ func Test7(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0, 1, 0, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{1, 1, 0}, Point{1, 3, 0}},
-			"5": []Point{Point{1, 1, 0}, Point{2, 1, 2}, Point{2, 4, 1}, Point{0, 4, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{1, 1, 0}, Point{1, 3, 0}}, ""},
+			"5": Path{"5", []Point{Point{1, 1, 0}, Point{2, 1, 2}, Point{2, 4, 1}, Point{0, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{0, 2, 0}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{0, 2, 0}, Point{0, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{1, 3, 0}, Point{1, 5, 1}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{1, 3, 0}, Point{1, 5, 1}, Point{0, 5, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{0, 4, 0}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{0, 4, 0}, Point{0, 5, 0}}, ""},
 		},
 	}
 
@@ -339,23 +339,23 @@ func Test8(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0, 0, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{1, 1, 0}, Point{1, 3, 1}, Point{0, 3, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{1, 1, 0}, Point{1, 3, 1}, Point{0, 3, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{0, 2, 0}, Point{0, 3, 0}},
-			"5": []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 3, 1}, Point{1, 3, 0} , Point{1, 4, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{0, 2, 0}, Point{0, 3, 0}}, ""},
+			"5": Path{"5", []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 3, 1}, Point{1, 3, 0} , Point{1, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{0, 3, 0}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{0, 3, 0}, Point{0, 5, 0}}, ""},
 		},
-		map[string][]Point{
-			"6": []Point{Point{1, 4, 0}, Point{1, 5, 1}, Point{0, 5, 0}},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{1, 4, 0}, Point{1, 5, 1}, Point{0, 5, 0}}, ""},
 		},
 	}
 
@@ -398,30 +398,30 @@ func Test9(t *testing.T) {
 	// Expected output
 	expectedColumns := []int{0, 1, 0, 0, 0, 2, 1, 0}
 
-	expectedPaths := []map[string][]Point{
-		map[string][]Point{
-			"3": []Point{Point{0, 0, 0}, Point{0, 2, 0}},
-			"2": []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}},
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, ""},
+			"2": Path{"2", []Point{Point{0, 0, 0}, Point{1, 0, 2}, Point{1, 1, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{1, 1, 0}, Point{1, 4, 1}, Point{0, 4, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{1, 1, 0}, Point{1, 4, 1}, Point{0, 4, 0}}, ""},
 		},
-		map[string][]Point{
-			"4": []Point{Point{0, 2, 0}, Point{0, 3, 0}},
-			"7": []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 4, 1}, Point{1, 4, 0}, Point{1, 6, 0}},
+		map[string]Path{
+			"4": Path{"4", []Point{Point{0, 2, 0}, Point{0, 3, 0}}, ""},
+			"7": Path{"7", []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 4, 1}, Point{1, 4, 0}, Point{1, 6, 0}}, ""},
 		},
-		map[string][]Point{
-			"5": []Point{Point{0, 3, 0}, Point{0, 4, 0}},
-			"6": []Point{Point{0, 3, 0}, Point{3, 3, 2}, Point{3, 4, 1}, Point{2, 4, 0}, Point{2, 5, 0}},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{0, 3, 0}, Point{0, 4, 0}}, ""},
+			"6": Path{"6", []Point{Point{0, 3, 0}, Point{3, 3, 2}, Point{3, 4, 1}, Point{2, 4, 0}, Point{2, 5, 0}}, ""},
 		},
-		map[string][]Point{
-			"8": []Point{Point{0, 4, 0}, Point{0, 7, 0}},
+		map[string]Path{
+			"8": Path{"8", []Point{Point{0, 4, 0}, Point{0, 7, 0}}, ""},
 		},
-		map[string][]Point{
-			"8": []Point{Point{2, 5, 0}, Point{2, 7, 1}, Point{0, 7, 0}},
+		map[string]Path{
+			"8": Path{"8", []Point{Point{2, 5, 0}, Point{2, 7, 1}, Point{0, 7, 0}}, ""},
 		},
-		map[string][]Point{
-			"8": []Point{Point{1, 6, 0}, Point{1, 7, 1}, Point{0, 7, 0}},
+		map[string]Path{
+			"8": Path{"8", []Point{Point{1, 6, 0}, Point{1, 7, 1}, Point{0, 7, 0}}, ""},
 		},
 	}
 
