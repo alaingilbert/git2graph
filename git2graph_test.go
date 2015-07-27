e5f3b4c52beb723b54430ae790ab0a0b80041d4b
Alain Gilbert <alain.gilbert.15@gmail.com>
2015-07-27 16:44:02 -0700

    test14

--- git2graph_test.go
+++ git2graph_test.go
@@ -664,3 +664,62 @@ func Test13(t *testing.T) {
 	validatePaths(t, expectedPaths, out)
 	validateColors(t, expectedPaths, out)
 }
+
+// 1
+// | 2
+// 3 |\
+// |\| |
+// | |\|
+// | | 4
+// | |/
+// | 5
+// 6 |\
+// |\| |
+// | |\|
+// | | 7
+// | |/
+// |/
+// 8
+func Test14(t *testing.T) {
+	// Initial input
+	inputNodes := make([]InputNode, 0)
+	inputNodes = append(inputNodes, InputNode{"1", []string{"3"}})
+	inputNodes = append(inputNodes, InputNode{"2", []string{"5", "4"}})
+	inputNodes = append(inputNodes, InputNode{"3", []string{"6", "4"}})
+	inputNodes = append(inputNodes, InputNode{"4", []string{"5"}})
+	inputNodes = append(inputNodes, InputNode{"5", []string{"8", "7"}})
+	inputNodes = append(inputNodes, InputNode{"6", []string{"8", "7"}})
+	inputNodes = append(inputNodes, InputNode{"7", []string{"8"}})
+	inputNodes = append(inputNodes, InputNode{"8", []string{}})
+
+	out, _ := buildTree(inputNodes, customColors)
+
+	// Expected output
+	expectedColumns := []int{0, 1, 0, 2, 1, 0, 2, 0}
+
+	expectedPaths := []map[string]Path{
+		map[string]Path{
+			"3": Path{"3", []Point{Point{0, 0, 0}, Point{0, 2, 0}}, "color1"},
+		},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{1, 1, 0}, Point{1, 4, 0}}, "color2"},
+			"4": Path{"4", []Point{Point{1, 1, 0}, Point{2, 1, 2}, Point{2, 3, 0}}, "color3"},
+		},
+		map[string]Path{
+			"6": Path{"6", []Point{Point{0, 2, 0}, Point{0, 5, 0}}, "color1"},
+			"4": Path{"4", []Point{Point{0, 2, 0}, Point{2, 2, 2}, Point{2, 3, 0}}, "color3"},
+		},
+		map[string]Path{
+			"5": Path{"5", []Point{Point{2, 3, 0}, Point{2, 4, 1}, Point{1, 4, 0}}, "color3"},
+		},
+		map[string]Path{
+			"8": Path{"8", []Point{Point{1, 4, 0}, Point{1, 7, 1}, Point{0, 7, 0}}, "color2"},
+			"7": Path{"7", []Point{Point{1, 4, 0}, Point{2, 4, 2}, Point{2, 6, 0}}, "color4"},
+		},
+	}
+
+	// Validation
+	validateColumns(t, expectedColumns, out)
+	validatePaths(t, expectedPaths, out)
+	validateColors(t, expectedPaths, out)
+}
