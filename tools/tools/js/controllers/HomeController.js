dcdfbd062077e8274089d67eae863365cb092988
Alain Gilbert <alain.gilbert.15@gmail.com>
2016-03-06 15:57:15 -0800

    App is now an angular app

---
+++ tools/tools/js/controllers/HomeController.js
@@ -0,0 +1,47 @@
+var app = angular.module('app', ['ngSanitize',]);
+
+app.controller('HomeController',
+  function($scope, $sce)
+  {
+
+    $scope.btnSaveClicked = function() {
+      localStorage.setItem('tree', JSON.stringify($scope.tree));
+    };
+
+    $scope.btnLoadClicked = function() {
+      var savedTree = localStorage.getItem('tree');
+      $scope.tree = JSON.parse(savedTree);
+    };
+
+
+    var highlightJson = function(json) {
+      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+            cls = 'key';
+          } else {
+            cls = 'string';
+          }
+        } else if (/true|false/.test(match)) {
+          cls = 'boolean';
+        } else if (/null/.test(match)) {
+          cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+      });
+    };
+
+    $scope.highlight = function(obj) {
+      return highlightJson(angular.toJson(obj, true))
+    };
+
+
+    (function constructor() {
+      $scope.selectedNode = null;
+      $scope.tree = [];
+    })();
+
+  }
+);
