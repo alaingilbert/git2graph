e5c5722090c7936dbaa31b8f23efaf653b077339
Alain Gilbert <alain.gilbert.15@gmail.com>
2016-03-06 22:34:36 -0800

    remove useless code

--- tools/tools/js/controllers/HomeController.js
+++ tools/tools/js/controllers/HomeController.js
@@ -109,27 +109,8 @@ app.controller('HomeController',
     };
 
 
-    var highlightJson = function(json) {
-      json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-      return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-          if (/:$/.test(match)) {
-            cls = 'key';
-          } else {
-            cls = 'string';
-          }
-        } else if (/true|false/.test(match)) {
-          cls = 'boolean';
-        } else if (/null/.test(match)) {
-          cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-      });
-    };
-
-    $scope.highlight = function(obj) {
-      return highlightJson(angular.toJson(obj, true))
+    $scope.toJson = function(tree) {
+      return angular.toJson(tree, true);
     };
 
 
